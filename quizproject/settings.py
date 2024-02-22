dfa32255a561cada2bc44af33652ca2b1ef2506c
Asad Sarfaraz <asadsarfraz474@gmail.com>
2024-02-22 20:08:40 +0530

    Renewing database

--- quizproject/settings.py
+++ quizproject/settings.py
@@ -39,9 +39,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['quiz-app-biin.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
